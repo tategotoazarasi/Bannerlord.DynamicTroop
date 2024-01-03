94f660bdea821301bec35153c9761be39f6f3a8a
Tategoto Azarasi <2724167997@qq.com>
2024-01-03 19:34:40 +0800

    No longer need horse on upgrade

---
+++ GetUpgradeRequiresItemFromCategoryPatch.cs
@@ -0,0 +1,20 @@
+﻿#region
+
+	using HarmonyLib;
+	using TaleWorlds.CampaignSystem;
+	using TaleWorlds.Core;
+
+#endregion
+
+	namespace Bannerlord.DynamicTroop;
+
+	[HarmonyPatch(typeof(CharacterObject), "get_UpgradeRequiresItemFromCategory")]
+	public class GetUpgradeRequiresItemFromCategoryPatch {
+		public static bool Prefix(CharacterObject __instance, ref ItemCategory __result) {
+			// 设置自定义返回值
+			__result = null;
+
+			// 返回 false 以阻止原始方法执行
+			return false;
+		}
+	}
