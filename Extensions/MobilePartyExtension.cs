1c86fb468957e55353bd401909579648102cd1f1
Tategoto Azarasi <2724167997@qq.com>
2024-02-18 19:34:56 +0800

    Update game settings by disabling AI crafting
    Game settings have been updated to disable the AI crafting functionality across different modules and strings. This change aims to prevent potential issues that could arise from the feature, such as increased file size and performance degradation.
    
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- Extensions/MobilePartyExtension.cs
+++ Extensions/MobilePartyExtension.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Bannerlord.DynamicTroop.Patches;
-
-using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
@@ -11,102 +9,98 @@ using TaleWorlds.LinQuick;
 
 namespace Bannerlord.DynamicTroop.Extensions;
 
-public static class MobilePartyExtension {
-	public static int GetClanTier(this MobileParty? party) {
-		if (!party.IsValid()) return 0;
+public static class MobilePartyExtension
+{
+    public static int GetClanTier(this MobileParty? party)
+    {
+        if (!party.IsValid()) return 0;
 
-		var hero = party?.Owner ?? party?.LeaderHero;
-		return hero != null ? hero.Clan?.Tier ?? 0 : 0;
-	}
+        var hero = party?.Owner ?? party?.LeaderHero;
+        return hero != null ? hero.Clan?.Tier ?? 0 : 0;
+    }
 
-	public static bool IsValid(this MobileParty? party) {
-		return party is { Id: { } } &&
-			   (EveryoneCampaignBehavior.PartyArmories.ContainsKey(party.Id) ||
-				party is {
-							 Owner       : { CharacterObject.IsHero: true, IsPartyLeader: true },
-							 LeaderHero  : { CharacterObject.IsHero: true, IsPartyLeader: true },
-							 MemberRoster: not null,
-							 Owner       : not { IsHumanPlayerCharacter: true }
-						 });
-	}
+    public static bool IsValid(this MobileParty? party)
+    {
+        return party is {Id: { }} &&
+               (EveryoneCampaignBehavior.PartyArmories.ContainsKey(party.Id) ||
+                party is
+                {
+                    Owner : {CharacterObject.IsHero: true, IsPartyLeader: true},
+                    LeaderHero : {CharacterObject.IsHero: true, IsPartyLeader: true},
+                    MemberRoster: not null,
+                    Owner : not {IsHumanPlayerCharacter: true}
+                });
+    }
 
-	public static List<EquipmentElement> GetItems(this MobileParty? party) {
-		List<EquipmentElement> listToReturn = new();
-		if (party == null) return listToReturn;
+    public static List<EquipmentElement> GetItems(this MobileParty? party)
+    {
+        List<EquipmentElement> listToReturn = new();
+        if (party == null) return listToReturn;
 
-		foreach (var element in party.MemberRoster.GetTroopRoster())
-			if (element.Character is { IsHero: false }) {
-				var list = RecruitmentPatch.GetRecruitEquipments(element.Character);
-				for (var i = 0; i < element.Number; i++) listToReturn.AddRange(list);
-			}
+        foreach (var element in party.MemberRoster.GetTroopRoster())
+            if (element.Character is {IsHero: false})
+            {
+                var list = RecruitmentPatch.GetRecruitEquipments(element.Character);
+                for (var i = 0; i < element.Number; i++) listToReturn.AddRange(list);
+            }
 
-		return listToReturn;
-	}
+        return listToReturn;
+    }
 
-	public static List<EquipmentElement> GetRandomEquipmentsFromTroop(this MobileParty? party) {
-		List<EquipmentElement> list = new();
-		if (party == null) return list;
+    public static List<EquipmentElement> GetRandomEquipmentsFromTroop(this MobileParty? party)
+    {
+        List<EquipmentElement> list = new();
+        if (party == null) return list;
 
-		var batchSize = 50 - (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0) * 10;
-		var cnt       = party.MemberRoster?.TotalManCount ?? 0;
-		var rosterWithoutHeroes = party.MemberRoster?.GetTroopRoster()
-									   ?.Where(member => !member.Character?.IsHero ?? false)
-									   ?.ToArrayQ();
-		if (rosterWithoutHeroes == null) return list;
+        var batchSize = 50 - (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0) * 10;
+        var cnt = party.MemberRoster?.TotalManCount ?? 0;
+        var rosterWithoutHeroes = party.MemberRoster?.GetTroopRoster()
+            ?.Where(member => !member.Character?.IsHero ?? false)
+            ?.ToArrayQ();
+        if (rosterWithoutHeroes == null) return list;
 
-		for (var i = 0; i <= cnt / batchSize; i++) {
-			var equipmentElement = rosterWithoutHeroes.GetRandomElement()
-													  .Character?.RandomBattleEquipment
-													  ?.GetEquipmentFromSlot(Global.EquipmentSlots.GetRandomElement());
-			if (equipmentElement is not { IsEmpty: false, Item: not null }) continue;
-			list.Add(equipmentElement.Value);
+        for (var i = 0; i <= cnt / batchSize; i++)
+        {
+            var equipmentElement = rosterWithoutHeroes.GetRandomElement()
+                .Character?.RandomBattleEquipment
+                ?.GetEquipmentFromSlot(Global.EquipmentSlots.GetRandomElement());
+            if (equipmentElement is not {IsEmpty: false, Item: not null}) continue;
+            list.Add(equipmentElement.Value);
+        }
 
-			if (ModSettings.Instance?.AiCrafting ?? false) {
-				var randomItem = Crafting.CreateRandomCraftedItem(party.LeaderHero?.Culture);
-				if (randomItem != null) {
-					CampaignEventDispatcher.Instance.OnNewItemCrafted(randomItem, null, false);
-					list.Add(new EquipmentElement(randomItem));
-				}
-			}
-		}
+        return list;
+    }
 
-		return list;
-	}
+    public static List<ItemObject> GetRandomEquipmentsFromClan(this MobileParty? party)
+    {
+        List<ItemObject> list = new();
+        if (party == null) return list;
 
-	public static List<ItemObject> GetRandomEquipmentsFromClan(this MobileParty? party) {
-		List<ItemObject> list = new();
-		if (party == null) return list;
+        var clanTier = party.GetClanTier() + (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0);
+        for (var i = 0; i <= clanTier; i++)
+        {
+            var items = Cache.GetItemsByTierAndCulture(clanTier,
+                party.Owner?.Clan?.Culture ?? party.LeaderHero?.Clan?.Culture);
+            if (items == null || items.IsEmpty()) continue;
 
-		var clanTier = party.GetClanTier() + (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0);
-		for (var i = 0; i <= clanTier; i++) {
-			var items = Cache.GetItemsByTierAndCulture(clanTier,
-													   party.Owner?.Clan?.Culture ?? party.LeaderHero?.Clan?.Culture);
-			if (items == null || items.IsEmpty()) continue;
+            list.Add(items.GetRandomElement());
+        }
 
-			list.Add(items.GetRandomElement());
-			if (ModSettings.Instance?.AiCrafting ?? false) {
-				var randomItem = Crafting.CreateRandomCraftedItem(party.LeaderHero?.Clan?.Culture);
-				if (randomItem != null) {
-					CampaignEventDispatcher.Instance.OnNewItemCrafted(randomItem, null, false);
-					list.Add(randomItem);
-				}
-			}
-		}
+        return list;
+    }
 
-		return list;
-	}
+    public static List<ItemObject> GetDailyEquipmentFromFiefs(this MobileParty? party)
+    {
+        List<ItemObject> list = new();
+        if (party == null) return list;
 
-	public static List<ItemObject> GetDailyEquipmentFromFiefs(this MobileParty? party) {
-		List<ItemObject> list = new();
-		if (party == null) return list;
+        MBReadOnlyList<Town>? fiefs = (party.LeaderHero?.Clan ?? party.Owner?.Clan)?.Fiefs;
+        if (fiefs == null) return list;
 
-		MBReadOnlyList<Town>? fiefs = (party.LeaderHero?.Clan ?? party.Owner?.Clan)?.Fiefs;
-		if (fiefs == null) return list;
+        foreach (var town in fiefs)
+            if (town.IsTown || town.IsCastle)
+                list.AddRange(town.GetRandomEquipments());
 
-		foreach (var town in fiefs)
-			if (town.IsTown || town.IsCastle)
-				list.AddRange(town.GetRandomEquipments());
-
-		return list;
-	}
+        return list;
+    }
 }
--- Extensions/TownExtension.cs
+++ Extensions/TownExtension.cs
@@ -1,47 +1,33 @@
 ﻿using System.Collections.Generic;
-
-using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
 
 namespace Bannerlord.DynamicTroop.Extensions;
 
-public static class TownExtension {
-	public static List<ItemObject> GetRandomEquipments(this Town? town) {
-		List<ItemObject> list = new();
-		if (town == null) return list;
+public static class TownExtension
+{
+    public static List<ItemObject> GetRandomEquipments(this Town? town)
+    {
+        List<ItemObject> list = new();
+        if (town == null) return list;
 
-		foreach (var village in town.Villages) {
-			if (village.VillageState != Village.VillageStates.Normal) continue;
+        foreach (var village in town.Villages)
+        {
+            if (village.VillageState != Village.VillageStates.Normal) continue;
 
-			var items = Cache.GetItemsByTierAndCulture(2 * (int)village.GetProsperityLevel() +
-													   (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0),
-													   town.Culture);
-			if (items != null && !items.IsEmpty()) list.Add(items.GetRandomElement());
-			if (ModSettings.Instance?.AiCrafting ?? false) {
-				var randomItem = Crafting.CreateRandomCraftedItem(town.Culture);
-				if (randomItem != null) { 
-					CampaignEventDispatcher.Instance.OnNewItemCrafted(randomItem, null, false);
-					list.Add(randomItem);
-				}
-			}
-		}
+            var items = Cache.GetItemsByTierAndCulture(2 * (int) village.GetProsperityLevel() +
+                                                       (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0),
+                town.Culture);
+            if (items != null && !items.IsEmpty()) list.Add(items.GetRandomElement());
+        }
 
-		if (town.IsUnderSiege) return list;
+        if (town.IsUnderSiege) return list;
 
-		var items2 =
-			Cache.GetItemsByTierAndCulture(2 * (int)town.GetProsperityLevel() +
-										   (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0),
-										   town.Culture);
-		if (items2 != null && !items2.IsEmpty()) list.Add(items2.GetRandomElement());
-		if (ModSettings.Instance?.AiCrafting ?? false) {
-			var randomItem2 = Crafting.CreateRandomCraftedItem(town.Culture);
-			if (randomItem2 != null) {
-				CampaignEventDispatcher.Instance.OnNewItemCrafted(randomItem2, null, false);
-				list.Add(randomItem2);
-			}
-		}
-		return list;
-	}
+        var items2 =
+            Cache.GetItemsByTierAndCulture(2 * (int) town.GetProsperityLevel() +
+                                           (ModSettings.Instance?.Difficulty.SelectedIndex ?? 0),
+                town.Culture);
+        if (items2 != null && !items2.IsEmpty()) list.Add(items2.GetRandomElement());
+        return list;
+    }
 }
