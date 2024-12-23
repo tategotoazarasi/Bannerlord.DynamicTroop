a8997ade4a0343f7772f190bca706836fd6d880a
Tategoto Azarasi <2724167997@qq.com>
2024-12-23 20:00:52 +0800

    init

---
+++ SubModule.cs
@@ -0,0 +1,21 @@
+﻿using TaleWorlds.MountAndBlade;
+
+
+namespace DTES2 {
+	public class SubModule : MBSubModuleBase {
+		protected override void OnSubModuleLoad() {
+			base.OnSubModuleLoad();
+
+		}
+
+		protected override void OnSubModuleUnloaded() {
+			base.OnSubModuleUnloaded();
+
+		}
+
+		protected override void OnBeforeInitialModuleScreenSetAsRoot() {
+			base.OnBeforeInitialModuleScreenSetAsRoot();
+
+		}
+	}
+}
